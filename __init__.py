6cdeaf0679b09ddbfc9a11e096dcbd233b86650c
Ahmet Alper <ahmetalpererdogan@gmail.com>
2024-08-25 15:17:41 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .modules.alper import Alper
+
+NODE_CLASS_MAPPINGS = {'Alperr' : Alper}
+
+NODE_DISPLAY_NAME_MAPPINGS = {'Alperr' : 'Alper Zort'}
+
+print('\033[35m')
+
+print('Ahmet Alper')
+
+print('\033[0m')
