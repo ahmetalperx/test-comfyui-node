180d94750980f983d9481af3acdc2e7b3618f40a
Ahmet Alper <ahmetalpererdogan@gmail.com>
2024-08-25 16:23:25 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
-from .nodes.alper import Alper
+from .modules.alper import Alper
+from .modules.ahmetalper import AhmetAlper
 
-NODE_CLASS_MAPPINGS = {'Alperr' : Alper}
+NODE_CLASS_MAPPINGS = {'Alperr' : Alper, 'Alperrr' : AhmetAlper}
 
-NODE_DISPLAY_NAME_MAPPINGS = {'Alperr' : 'Alper Zort'}
+NODE_DISPLAY_NAME_MAPPINGS = {'Alperr' : 'Alper Zort', 'Alperrr' : AhmetAlper}
 
 print('\033[35m')
 
